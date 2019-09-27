5ff355126303dc9c896dea2ccac74ad1f6d304b2
juming <juming@touscm.com>
2019-09-27 22:26:55 +0800

    change token name

--- IE_Token.sol
+++ IE_Token.sol
@@ -10,12 +10,12 @@ import "./ERC20Detailed.sol";
  * `ERC20` functions.
  */
 contract IE_Token is ERC20, ERC20Detailed {
-    uint256 public constant INITIAL_SUPPLY = 2100000000 * (10 ** 8);
+    uint256 public constant INITIAL_SUPPLY = 210000000 * (10 ** 8);
 
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public ERC20Detailed("IE：Intelligent education", "IECOIN", 8) {
+    constructor () public ERC20Detailed("Intelligent education", "IE COIN", 8) {
         _mint(msg.sender, INITIAL_SUPPLY);
     }
 }
