26291f988c66bbd646466aac4730c40543b23728
juming <juming@touscm.com>
2019-09-27 18:09:15 +0800

    init

---
+++ IE_Token.sol
@@ -0,0 +1,21 @@
+pragma solidity >=0.4.24 <0.6.0;
+
+import "./ERC20.sol";
+import "./ERC20Detailed.sol";
+
+/**
+ * @title IE_Token
+ * @dev Very simple ERC20 Token example, where all tokens are pre-assigned to the creator.
+ * Note they can later distribute these tokens as they wish using `transfer` and other
+ * `ERC20` functions.
+ */
+contract IE_Token is ERC20, ERC20Detailed {
+    uint256 public constant INITIAL_SUPPLY = 2100000000 * (10 ** 8);
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     */
+    constructor () public ERC20Detailed("IE：Intelligent education", "IECOIN", 8) {
+        _mint(msg.sender, INITIAL_SUPPLY);
+    }
+}
